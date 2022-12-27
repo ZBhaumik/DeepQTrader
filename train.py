8a774224e471aef2db5f711ddb708d019e402859
Zubin Bhaumik <113847258+ZBhaumik@users.noreply.github.com>
2022-12-26 22:12:54 -0800

    YFinance Implementation
    Added calls to YFinance API instead of using .csv data. Also, added model training parameters in train.py main() to enable model retraining

--- train.py
+++ train.py
@@ -5,20 +5,15 @@ from docopt import docopt
 
 def main(train_stock, val_stock, window_size, batch_size, ep_count,
          strategy="t-dqn", model_name="model_debug", pretrained=False,
-         debug=False):
-    """ Trains the stock trading bot using Deep Q-Learning.
-    Please see https://arxiv.org/abs/1312.5602 for more details.
-
-    Args: [python train.py --help]
-    """
+         debug=False, init_episode = 1):
     agent = Agent(window_size, strategy=strategy, pretrained=pretrained, model_name=model_name)
     
-    train_data = get_stock_data(train_stock)
-    val_data = get_stock_data(val_stock)
+    train_data = yfinance_retrieve(train_stock, 0)
+    val_data = yfinance_retrieve(val_stock, 1)
 
     initial_offset = val_data[1] - val_data[0]
 
-    for episode in range(1, ep_count + 1):
+    for episode in range(init_episode, ep_count + 1):
         train_result = train_model(agent, episode, train_data, ep_count=ep_count,
                                    batch_size=batch_size, window_size=window_size)
         val_result, _ = evaluate_model(agent, val_data, window_size, debug)
@@ -26,5 +21,5 @@ def main(train_stock, val_stock, window_size, batch_size, ep_count,
 
 
 coloredlogs.install(level="DEBUG")
-#switch_k_backend_device()
-main("AMZN.csv","AMZN.csv", window_size=10, batch_size=32, ep_count=100)
+switch_k_backend_device()
+main("MSFT","MSFT", window_size=10, batch_size=32, ep_count=10, model_name="model_debug_5", pretrained=True, init_episode=5)
--- utilities.py
+++ utilities.py
@@ -2,15 +2,26 @@
 import os
 import math
 import logging
+
 import pandas as pd
 import numpy as np
+
 from tqdm import tqdm
 import keras.backend as K
+import yfinance as yf
 
+
+# Formats Position
 format_position = lambda price: ('-$' if price < 0 else '+$') + '{0:.2f}'.format(abs(price))
+
+
+# Formats Currency
 format_currency = lambda price: '${0:.2f}'.format(abs(price))
 
+
 def show_train_result(result, val_position, initial_offset):
+    """ Displays training results
+    """
     if val_position == initial_offset or val_position == 0.0:
         logging.info('Episode {}/{} - Train Position: {}  Val Position: USELESS  Train Loss: {:.4f}'
                      .format(result[0], result[1], format_position(result[2]), result[3]))
@@ -18,19 +29,27 @@ def show_train_result(result, val_position, initial_offset):
         logging.info('Episode {}/{} - Train Position: {}  Val Position: {}  Train Loss: {:.4f})'
                      .format(result[0], result[1], format_position(result[2]), format_position(val_position), result[3],))
 
+
 def show_eval_result(model_name, profit, initial_offset):
+    """ Displays eval results
+    """
     if profit == initial_offset or profit == 0.0:
         logging.info('{}: USELESS\n'.format(model_name))
     else:
         logging.info('{}: {}\n'.format(model_name, format_position(profit)))
 
-def get_stock_data(stock_file):
-    """Reads stock data from csv file
-    """
-    df = pd.read_csv(stock_file)
+dates = ["2016-10-01","2017-01-01","2017-01-02","2018-01-02","2018-01-03","2019-01-03"]
+def yfinance_retrieve(stock_name, type):
+    type=type*2
+    df = yf.download(stock_name, start=dates[type], end=dates[type+1])
     return list(df['Adj Close'])
 
+
 def switch_k_backend_device():
+    """ Switches `keras` backend from GPU to CPU if required.
+
+    Faster computation on CPU (if using tensorflow-gpu).
+    """
     if K.backend() == "tensorflow":
         logging.debug("switching to TensorFlow for CPU")
         os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
